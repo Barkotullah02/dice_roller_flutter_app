b48411ca39078597afe2b858c5b198460c32a05b
Barkeotullah Opu <146110521+Barkotullah02@users.noreply.github.com>
2024-09-26 22:13:07 +0600

    added a const modifies

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
           title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
       debugShowCheckedModeBanner: false,
     ),
